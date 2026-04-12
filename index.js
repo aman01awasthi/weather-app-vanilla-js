9a2f26a44b4c15bac8e739af5960730242863082
Amandeep Awasthi <aman01awasthi@gmail.com>
2026-04-13 02:12:03 +0530

    feat: weather app with fetch, error handling, and localStorage

---
+++ index.js
@@ -0,0 +1,95 @@
+// search button click event listener with arrow function so that inside it we can call showLoader(),getWeather()
+// getWeather() -> in async/await with try catch will try to fetch data else with catch will catch error
+// PURPOSE-> return api data resolved withing try or throw error in catch 
+// RECEIVES-> users city
+// RETURNS-> Weather data of user eneterd city
+// showLoader conveys user that data is being fetched untill then
+// PURPOSE-> to let user wait untill DOM is ready to display 
+// RETURNS-> it stops after that
+// renderData()-> create DOM and then also modify it's textContent
+// PURPOSE-> create and display DOM  
+// RECEIVES-> textcontent of data which needs to be displayed
+// RETURNS-> HTML elements
+// saveData() -> to save data in localStorage
+// loadData() -> to get data from localStorage
+let id = `b5250823e2c02e80a91ca40a93d5c152`;
+let message;
+loadLastCity();
+
+async function loadLastCity(){
+    let savedCity = loadData();
+    document.getElementById('city').value = savedCity;
+    let savedCityData = await getWeather(savedCity);
+
+    if(savedCity){
+        renderData(savedCityData);
+    }else{
+        return '';
+    }
+}
+
+
+async function getWeather(city) {
+    try{
+        // fetching data and stored in variable to convert it to required format 
+        let res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${id}&units=metric`);
+
+        // here we wait untill the dom is ready properly and also convert data to js object here
+        let response = await res.json();
+
+        if(response.cod === "404"){
+            message = "City Not Found";
+            return message;
+        }
+
+        return response;
+    }catch(err){
+        //here we show or catch erro
+        return err.message;
+    } 
+}
+
+document.getElementById('weatherForm').addEventListener('submit', async (e)=>{
+    e.preventDefault();
+    let cityVal = document.getElementById('city').value.trim();
+    // console.log(cityVal);
+    let dataErr = document.getElementById('displayError');
+    if(cityVal === ""){
+        message = "Empty Input";
+        return dataErr.textContent = message;
+    }
+    let data = await getWeather(cityVal);
+    // console.log(typeof data);
+    if(typeof data === 'object'){
+        renderData(data);
+        dataErr.textContent = '';
+        console.log(saveData(cityVal));
+    } else{
+        dataErr.textContent = data;
+    }
+})
+
+// create DOM and will display response in list
+function renderData(response){
+    let displayData = document.getElementById('weather-container');
+    displayData.textContent = '';
+
+    let li = document.createElement('li');
+    li.textContent = `${response.name} - ${response.main.temp} - ${response.weather[0].description} - ${response.main.humidity} - ${response.wind.speed}`;
+
+    displayData.appendChild(li);
+}
+
+function saveData(cityVal){
+    localStorage.setItem('city', cityVal);
+}
+
+function loadData(){
+    let sotredCity = localStorage.getItem('city');
+
+    if(sotredCity){
+        return sotredCity;
+    }else{
+        return '';
+    }
+}
